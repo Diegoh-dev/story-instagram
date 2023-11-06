7db2632c95aa0713d6186ebee987b4305bdcaab3
Diego ribeiro <diego@sistemabiodata.com.br>
2023-11-06 13:51:44 -0300

    iniciando slide instagram.

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+import Slide from "./Slide.js";
+const container = document.getElementById('slide');
+const elements = document.querySelectorAll('#slide-elements');
+const controls = document.getElementById('slide-controls');
+if (container && elements && controls) {
+    new Slide(container, Array.from(elements), controls, 3000);
+}
+//# sourceMappingURL=script.js.map
